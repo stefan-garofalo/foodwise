0c79595a8dd5aea2f84b1c3067fa13d679256a64
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-05-03 16:55:41 +0200

    fix(packages/api): base procedures lib

--- packages/api/lib/index.ts
+++ packages/api/lib/index.ts
@@ -1,31 +1,32 @@
 import { z } from 'zod'
 import { err, ok } from 'neverthrow'
-import { eq, inArray } from 'drizzle-orm'
+import { eq, inArray } from '@repo/db/utils'
 
 import {
 	createInsertSchema,
 	createSelectSchema,
-	createUpdateSchema
+	createUpdateSchema,
+	toSelectFields
 } from '@repo/db/schema/utils'
 import { authedProcedure } from '../trpc'
 
 import { BaseSQLiteTable } from '@repo/db/utils'
-import { SQLiteUpdateSetSource } from 'drizzle-orm/sqlite-core'
+import { SQLiteTable, SQLiteUpdateSetSource } from 'drizzle-orm/sqlite-core'
 
 export function createBaseProcedures<TTable extends BaseSQLiteTable>(
 	table: TTable
 ) {
 	return {
 		create: authedProcedure
-			.input(createInsertSchema(table))
+			.input(createInsertSchema(table as unknown as SQLiteTable))
 			.mutation(async ({ input, ctx: { db } }) => {
 				try {
 					const query = await db
-						.insert(table)
+						.insert(table as unknown as BaseSQLiteTable)
 						.values(input as TTable['$inferInsert'])
 						.onConflictDoUpdate({
-							target: table.id,
-							set: input as SQLiteUpdateSetSource<TTable>
+							target: (table as unknown as BaseSQLiteTable).id,
+							set: input as SQLiteUpdateSetSource<SQLiteTable>
 						})
 						.returning()
 					return ok(Array.isArray(input) ? query : query[0])
@@ -37,14 +38,14 @@ export function createBaseProcedures<TTable extends BaseSQLiteTable>(
 		update: authedProcedure
 			.input(
 				z.object({
-					values: createUpdateSchema<TTable>(table),
+					values: createUpdateSchema<SQLiteTable>(table as unknown as SQLiteTable),
 					id: z.string()
 				})
 			)
 			.mutation(async ({ input, ctx: { db } }) => {
 				try {
 					const query = await db
-						.update(table)
+						.update(table as unknown as BaseSQLiteTable)
 						.set({ ...input?.values!, updatedAt: new Date() })
 						.where(eq(table.id, input?.id!))
 						.returning()
@@ -67,32 +68,38 @@ export function createBaseProcedures<TTable extends BaseSQLiteTable>(
 				}
 			}),
 
-		get: authedProcedure
-			.input(
-				z.object({
-					id: z.string(),
-					columns: createSelectSchema(table)
-				})
-			)
-			.query(async ({ input: { id, columns }, ctx: { db } }) => {
-				try {
-					return ok(
-						await db.query[table._.name].findFirst({
-							columns,
-							where: (table, { eq }) => eq(table.id, id)
-						})
-					)
-				} catch (error) {
-					return err(error)
-				}
-			}),
+		get: authedProcedure.query(async ({ ctx: { db } }) => {}),
+		getAll: authedProcedure.query(async ({ ctx: { db } }) => {})
+		// get: authedProcedure
+		// 	.input(
+		// 		z.object({
+		// 			id: z.string(),
+		// 			columns: createSelectSchema(table)
+		// 		})
+		// 	)
+		// 	.query(async ({ input: { id, columns }, ctx: { db } }) => {
+		// 		// db.query.categories.findFirst({
+		// 		// 	columns: toSelectFields(columns),
+		// 		// 	where: (table, { eq }) => eq(table.id, id)
+		// 		// })
+		// 		try {
+		// 			return ok(
+		// 				await db.query[table._.name].findFirst({
+		// 					columns: toSelectFields(columns),
+		// 					where: (t: TTable, { eq }: { eq: typeof eq }) => eq(t.id, id)
+		// 				})
+		// 			)
+		// 		} catch (error) {
+		// 			return err(error)
+		// 		}
+		// 	}),
 
-		getAll: authedProcedure.query(async ({ ctx: { db } }) => {
-			try {
-				return ok(await db.query[table._.name].findMany({}))
-			} catch (error) {
-				return err(error)
-			}
-		})
+		// getAll: authedProcedure.query(async ({ ctx: { db } }) => {
+		// 	try {
+		// 		return ok(await db.query[table._.name].findMany({}))
+		// 	} catch (error) {
+		// 		return err(error)
+		// 	}
+		// })
 	}
 }
