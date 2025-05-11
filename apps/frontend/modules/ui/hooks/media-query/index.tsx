a76f83f12630e3740037c342c11452190da2c114
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-05-11 23:29:06 +0200

    fix(frontend): build err

--- apps/frontend/modules/ui/hooks/media-query/index.tsx
+++ apps/frontend/modules/ui/hooks/media-query/index.tsx
@@ -8,6 +8,16 @@ export function useIsDesktop() {
 	return useMediaQuery('(min-width: 769px)')
 }
 
+export function MobileMediaQuery({ children }: { children: React.ReactNode }) {
+	const isMobile = useIsMobile()
+	return isMobile ? children : null
+}
+
+export function DesktopMediaQuery({ children }: { children: React.ReactNode }) {
+	const isDesktop = useIsDesktop()
+	return isDesktop ? children : null
+}
+
 function useMediaQuery(query: string) {
 	const subscribe = useCallback(
 		(callback: (event: MediaQueryListEvent) => void) => {
