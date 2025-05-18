b255c4b224accbd0125a92864deb9eab862c33e6
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-05-18 11:31:48 +0200

    fix(frontend): middleware relative path 500

--- apps/frontend/utils/middleware.ts
+++ apps/frontend/utils/middleware.ts
@@ -1,4 +1,12 @@
+import { NextRequest } from 'next/server'
+
 // Helper function to check if a path is a static file
 export function isStaticFile(pathname: string) {
 	return Boolean(pathname.match(/\.[^/]+$/))
+}
+
+export function setAbsoluteUrl(request: NextRequest, path: string) {
+	const url = request.nextUrl.clone()
+	url.pathname = path
+	return url
 }
