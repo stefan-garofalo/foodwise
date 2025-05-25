fc6510ae3ac9a86c156a95393c14f8697373caad
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-05-25 14:46:06 +0200

    fix(frontend/middleware): missing lang from cookie as truth source when redirecting from protected route

--- apps/frontend/modules/auth/middleware/index.ts
+++ apps/frontend/modules/auth/middleware/index.ts
@@ -4,16 +4,23 @@ import { PUBLIC_ROUTES } from '../config'
 import { stripLangFromPathname } from '@/modules/i18n/middleware'
 import { buildAbsoluteUrl } from '@/utils/middleware'
 import { FALLBACK_LOCALE } from '@/modules/i18n/config'
+import { getLocaleFromCookie } from '@/modules/i18n/lib/cookies'
 
 export function canAccessRoute(request: NextRequest) {
 	const session = getSessionCookie(request)
 	const [lang, path] = stripLangFromPathname(request.nextUrl.pathname)
-	console.log({ lang, path })
+
 	if (session && PUBLIC_ROUTES.includes(path)) {
-		return buildAbsoluteUrl(`/${lang ?? FALLBACK_LOCALE}`, request)
+		return buildAbsoluteUrl(
+			`/${lang ?? getLocaleFromCookie(request.cookies)}`,
+			request
+		)
 	}
 	if (!session && !PUBLIC_ROUTES.includes(path)) {
-		return buildAbsoluteUrl(`/${lang ?? FALLBACK_LOCALE}/login`, request)
+		return buildAbsoluteUrl(
+			`/${lang ?? getLocaleFromCookie(request.cookies)}/login`,
+			request
+		)
 	}
 
 	return null
