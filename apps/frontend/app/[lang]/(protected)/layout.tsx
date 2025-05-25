eb8e7bbcdd7723db5ba7d94966672b1962249352
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-05-25 15:44:12 +0200

    feat(frontend): initial sidebar deps

--- apps/frontend/app/[lang]/(protected)/layout.tsx
+++ apps/frontend/app/[lang]/(protected)/layout.tsx
@@ -1,12 +1,13 @@
 import { Header } from '@/features/layout/header'
-import { Sidebar } from '@/features/layout/sidebar'
+import LayoutSidebar from '@/features/layout/sidebar'
+
 import { SidebarInset, SidebarProvider } from '@/modules/ui/primitives/sidebar'
 import { PropsWithChildren } from 'react'
 
 export default function ProtectedLayout({ children }: PropsWithChildren) {
 	return (
 		<SidebarProvider>
-			<Sidebar variant="inset" />
+			<LayoutSidebar />
 			<SidebarInset>
 				<Header />
 				{children}
