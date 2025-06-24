c7541dc8372f9de0286678b33ebcf9b1f482a10d
stefan-garofalo <stefan.garofalo@devpunks.com>
2025-06-24 16:26:29 +0200

    feat(frontend/modules): tanstack form implementation

--- apps/frontend/modules/ui/primitives/input.tsx
+++ apps/frontend/modules/ui/primitives/input.tsx
@@ -2,27 +2,25 @@ import * as React from 'react'
 
 import { merge } from '@/modules/ui/utils/tailwind'
 
-const Input = (
-    {
-        ref,
-        className,
-        type,
-        ...props
-    }: React.ComponentProps<'input'> & {
-        ref: React.RefObject<HTMLInputElement>;
-    }
-) => {
-    return (
-        <input
-            type={type}
-            className={merge(
-                'border-input file:text-foreground placeholder:text-foreground/20 focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-                className
-            )}
-            ref={ref}
-            {...props}
-        />
-    )
+const Input = ({
+	ref,
+	className,
+	type,
+	...props
+}: React.ComponentProps<'input'> & {
+	ref: React.RefObject<HTMLInputElement>
+}) => {
+	return (
+		<input
+			type={type}
+			className={merge(
+				'border-input file:text-foreground placeholder:text-foreground/20 focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+				className
+			)}
+			ref={ref}
+			{...props}
+		/>
+	)
 }
 Input.displayName = 'Input'
 
